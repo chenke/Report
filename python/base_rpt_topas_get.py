54093ea6e9c1b5ed09a363ba55f20031aca001ed
chenke <chenke@maimiaotech.com>
2012-11-24 20:33:42 +0800

    省油宝报表库提交

--- python/base_rpt_topas_get.py
+++ python/base_rpt_topas_get.py
@@ -61,9 +61,10 @@ class BaseRptTopatsGet():
         """
         for rpt_data in self.rpt_data_list:
             rpt_data_normal = rpt_data[rpt_data.find('['):rpt_data.find(']')+1]
-            if len(rpt_data_normal) <= 10:
+            try:
+                one_rpt_record_list = json.loads(rpt_data_normal.lower())
+            except Exception, data:
                 continue
-            one_rpt_record_list = json.loads(rpt_data_normal.lower())
             one_rpt_record_list_filter = []
             for element in one_rpt_record_list:
                 if len(element.keys()) >= 13:
